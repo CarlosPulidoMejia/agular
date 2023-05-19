84e767f8c2dcfbbf4d5bbba2f2af273b0e1465ea
CarlosPulidoMejia <cjlpulido@hotmail.com>
2023-05-19 16:54:30 -0600

    Inicio Output

--- src/app/Components/saludo/saludo.component.ts
+++ src/app/Components/saludo/saludo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
@@ -8,13 +8,18 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SaludoComponent implements OnInit{
 
   @Input() nombre: string = "Anonimo";  
+  //@Output () mensajeEmitter: EventEmitter string = new EventEmitter
     
   constructor() {}
   
   ngOnInit(): void {
     //Instrucciones previas a la renderización del componente
     console.log('ngOnInit del componente Saludo');
-    
+  }
+
+  /*Ejemplo para festionar un even de tipo clic en el DOM */
+  alertaSaludo(): void{
+    alert(`Hola, ${this.nombre}. Alerta desde un clic de botón`)
   }
 
 }

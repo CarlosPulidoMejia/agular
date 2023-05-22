d4752de3b026fd69584cede126f302ecd829424c
CarlosPulidoMejia <cjlpulido@hotmail.com>
2023-05-22 17:33:34 -0600

    ngIf

--- src/app/modules/list/lista-basica/lista-basica.component.ts
+++ src/app/modules/list/lista-basica/lista-basica.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-basica',
   templateUrl: './lista-basica.component.html',
   styleUrls: ['./lista-basica.component.scss']
 })
-export class ListaBasicaComponent {
+export class ListaBasicaComponent implements OnInit {
+
+
+  listaElementos: string[] = ["Leche", "Carne", "Verdura", "Huevos"];
+  cargando: boolean = false;
+
+  ngOnInit(): void{
+
+  }
+
+  cambiarCargadno(){
+    this.cargando = !this.cargando
+  }
 
 }

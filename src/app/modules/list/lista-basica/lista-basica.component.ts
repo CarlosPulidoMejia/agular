80a428fc8bc31484cd8dfa8b64a8fcb0ac8d8aae
CarlosPulidoMejia <cjlpulido@hotmail.com>
2023-05-23 10:06:28 -0600

    ngFor

--- src/app/modules/list/lista-basica/lista-basica.component.ts
+++ src/app/modules/list/lista-basica/lista-basica.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+export type Producto = {
+  nombre: string;
+  precio: number;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-lista-basica',
   templateUrl: './lista-basica.component.html',
   styleUrls: ['./lista-basica.component.scss']
 })
+
 export class ListaBasicaComponent implements OnInit {
 
 
-  listaElementos: string[] = ["Leche", "Carne", "Verdura", "Huevos"];
+  listaElementos: Producto[] = [
+    {
+      nombre:"Leche",
+      precio: 10,
+      descripcion: "Leche entera"
+    },
+    {
+      nombre:"Carne",
+      precio: 20,
+      descripcion: "Carne De puerco"
+    }
+  ];
   cargando: boolean = false;
 
   ngOnInit(): void{
